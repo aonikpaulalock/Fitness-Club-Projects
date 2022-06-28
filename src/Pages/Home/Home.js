1fac16c2ccc15d75e67b988490bf6a96b584ac55
aonikpaulalock <alockpaul9845@gmail.com>
2022-06-28 16:06:06 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../../Styles/Banner.css"
+const Home = () => {
+  return (
+    <div>
+      <h1 className="secondary">Primary Color</h1>
+    </div>
+  );
+};
+
+export default Home;
